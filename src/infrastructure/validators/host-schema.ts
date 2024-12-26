6c6a46db2bcebc8694482f1f3447e0d761d3f661
Binshad K <binshadcs@gmail.com>
2024-12-26 17:26:27 +0530

    refactor

--- src/infrastructure/validators/host-schema.ts
+++ src/infrastructure/validators/host-schema.ts
@@ -6,7 +6,7 @@ export const HostRegisterValidate = z.object({
     .min(3, { message: "Must be 3 or fewer characters long" }),
   lastName: z.string().optional(),
   email: z.string().email({ message: "must be a email" }),
-  phone: z.number({ message: "Invalid phone number" }),
+  phone: z.string({ message: "Invalid phone number" }),
   profile: z.string().optional(),
   password: z.string({ message: "invalid password" }),
 });
